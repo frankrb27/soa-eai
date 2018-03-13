231cf94fd0ffd647186b5bca1998fe35b0c180bb
frankr27 <frank.rb27@hotmail.com>
2018-03-13 00:54:32 -0500

    enum

--- src/main/java/co/com/ws/processor/MaintenancePlannerProcessor.java
+++ src/main/java/co/com/ws/processor/MaintenancePlannerProcessor.java
@@ -31,7 +31,7 @@ public class MaintenancePlannerProcessor implements Processor {
 			request.setEndDate(requestMain.getEndDate());
 			request.setExternalKey(requestMain.getFlightExternalKey());
 			request.setStartDate(requestMain.getStartDate());
-			request.setStatus(FlightUpdateStatus.valueOf(requestMain.getStatus()));
+			request.setStatus(FlightUpdateStatus.valueOf(requestMain.getStatus().equals("IN_PROGRESS")?"ON_GOING":requestMain.getStatus()));
 			
 			FlightsUpdateApiService service = new FlightsUpdateApiService();
 			FlightsUpdateApi port = service.getFlightsUpdateApiPort();
